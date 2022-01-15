6681c3ae150e60cbbc646472ddff4ad6396fbec1
Minji <minji.jung@voithru.com>
2022-01-15 14:37:24 +0900

    style: Black
    Refs: #1

--- problems/array_and_string/learn/largest_at_least_twice.py
+++ problems/array_and_string/learn/largest_at_least_twice.py
@@ -2,7 +2,8 @@
 https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1147/
 
 You are given an integer array nums where the largest integer is unique.
-Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+Determine whether the largest element in the array is at least twice as much as every other number in the array.
+If it is, return the index of the largest element, or return -1 otherwise.
 
 Example 1:
 Input: nums = [3,6,1,0]
@@ -26,7 +27,8 @@ The largest element in nums is unique.
 
 class Solution:
     def dominantIndex(self, nums: List[int]) -> int:
-        if len(nums) == 1: return 0
+        if len(nums) == 1:
+            return 0
         largest = max(nums)
 
         for idx, num in enumerate(nums):
