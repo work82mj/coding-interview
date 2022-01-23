4fb1c7add37c5d191f0cf7a66205538466e2aca5
Minji <minji.jung@voithru.com>
2022-01-23 22:28:46 +0900

    feat: Add rotate array
    Refs: #1

---
+++ problems/array_and_string/learn/rotate_array.py
@@ -0,0 +1,32 @@
+"""
+https://leetcode.com/explore/learn/card/array-and-string/204/conclusion/1182/
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+
+Example 2:
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+
+Constraints:
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+"""
+
+# TLE
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        for i in range(k):
+            right = len(nums) - 1
+            while right != 0:
+                nums[right], nums[right - 1] = nums[right - 1], nums[right]
+                right -= 1
