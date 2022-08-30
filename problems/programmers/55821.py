964ea8b784a6786fd3a82ca45517a8197030a83e
Minji <minji.jung@voithru.com>
2022-08-30 19:48:57 +0900

    feat: Add solution2
    Refs: #5 #programmers #greedy

--- problems/programmers/55821.py
+++ problems/programmers/55821.py
@@ -3,7 +3,7 @@ https://school.programmers.co.kr/learn/courses/9899/lessons/55821
 """
 
 # there is one error case
-def solution(n, lost, reserve):
+def solution1(n, lost, reserve):
     tmp = reserve.copy()
     ret = 0
     for l in lost:
@@ -18,3 +18,15 @@ def solution(n, lost, reserve):
             tmp.pop(tmp.index(l + 1))
 
     return n - len(lost) + ret
+
+def solution2(n, lost, reserve):
+    set_lost = set(lost) - set(reserve)
+    set_reserve = set(reserve) - set(lost)
+    ret = 0
+    for i in set_reserve:
+        if i - 1 in set_lost:
+            set_lost.remove(i - 1)
+        elif i + 1 in set_lost:
+            set_lost.remove(i + 1)
+
+    return n - len(set_lost)
